48f3c4be727a864944b6db58c49554f5335a88b4
David Andersson <lejonmanen@gmail.com>
2024-11-13 10:01:20 +0100

    7.1

---
+++ script.js
@@ -0,0 +1,10 @@
+// 7.1
+
+const element = document.querySelector('.dynamic')
+element.innerText = 'Hello world'
+element.innerText = 'Dynamisk webbsida'
+
+// className ersätter alla klasser
+// element.className = 'frame'
+// classList.add lägger till en ny klass
+element.classList.add('frame')
