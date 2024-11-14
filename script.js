03217582b76259b944c02c043fa4f93893a6d187
David Andersson <lejonmanen@gmail.com>
2024-11-14 14:34:55 +0100

    7.6

--- script.js
+++ script.js
@@ -64,3 +64,84 @@ Exempel på algoritm för övning 7.3
 
 + addEventListener för +1 och -1 knapparna
 */
+
+
+// 7.6
+// 6a Bygg en sida med ett trafikljus. När man klickar på en knapp ska nästa lampa i serien tändas.
+// Rött → gult → grönt.
+
+// Deklarera variabel - för den tända lampan
+const RED = 0, RED_YELLOW = 1, GREEN = 2, YELLOW = 3
+let lampState = RED  // tillståndet 0 är rött ljus, 1 är gult, 2 är grönt
+
+const lightButton = document.querySelector('#light-button')
+const top = document.querySelector('.lights .top')
+const middle = document.querySelector('.lights .middle')
+const bottom = document.querySelector('.lights .bottom')
+// console.log('DOM elements: ', lightButton, top, middle, bottom)
+
+lightButton.addEventListener('click', () => {
+	lampState = lampState + 1
+	if( lampState === RED_YELLOW ) {
+		// top.classList.remove('on') <- släcks först när ljuset slår över till grönt
+		middle.classList.add('on')
+	}
+	else if( lampState === GREEN ) {
+		top.classList.remove('on')
+		middle.classList.remove('on')
+		bottom.classList.add('on')
+	}
+	else if( lampState === YELLOW ) {
+		bottom.classList.remove('on')
+		middle.classList.add('on')
+	}
+	else if( lampState > YELLOW ) {
+		lampState = RED
+		middle.classList.remove('on')
+		top.classList.add('on')
+	}
+})
+
+
+// Alternativ 1
+/*
+let lamps = document.querySelectorAll(".lights > *")
+let lampRed = document.querySelector(".top");
+let lampYellow = document.querySelector(".middle");
+let lampGreen = document.querySelector(".bottom");
+
+lampRed.addEventListener("mouseover",()=>{
+	lampRed.style.backgroundColor="red"
+	lamps[1].style.backgroundColor="";
+	lamps[2].style.backgroundColor="";
+})
+lampYellow.addEventListener("mouseover",()=>{
+	lamps[0].style.backgroundColor="";
+	lamps[2].style.backgroundColor="";
+	lampYellow.style.backgroundColor="yellow"
+})
+lampGreen.addEventListener("mouseover",()=>{
+	lamps[1].style.backgroundColor="";
+	lamps[0].style.backgroundColor="";
+	lampGreen.style.backgroundColor="green"
+})
+*/
+
+// Alternativ 2
+/*
+let lamps = document.querySelectorAll(".lights > *");
+lamps.forEach((lamp, index) => {
+	lamp.addEventListener("mouseover",()=>{
+		lamps.forEach(la => la.style.backgroundColor="")
+		if(index===0){
+			lamp.style.backgroundColor="red";
+		}else if(index===1){
+			lamp.style.backgroundColor="yellow";
+
+		}else if(index===2){
+			lamp.style.backgroundColor="green";
+		}
+	})
+})
+*/
+
